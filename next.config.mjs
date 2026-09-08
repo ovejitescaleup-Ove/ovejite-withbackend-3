111b1bc28b3b6f23d211a89159a8a9e49d09ccb0
OVEJITE KUMAR VHOWMICK <ovejite.scaleup@gmail.com>
2026-09-08 21:50:09 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  pageExtensions: [
-    "page.js",
-    "page.jsx",
-    "page.ts",
-    "page.tsx",
-    "layout.js",
-    "layout.jsx",
-    "layout.ts",
-    "layout.tsx",
-  ],
 };
 
 export default nextConfig;
