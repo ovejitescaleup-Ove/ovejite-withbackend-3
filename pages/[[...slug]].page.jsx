44ac2528d5f2da3ee465a0d600ddf5fe75a975d4
OVEJITE KUMAR VHOWMICK <ovejite.scaleup@gmail.com>
2026-09-10 12:08:26 +0600

    Delete pages directory

--- pages/[[...slug]].page.jsx
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-const ReactApp = dynamic(() => import("../src/App"), {
-  ssr: false,
-});
-
-export default function CatchAllPage() {
-  return <ReactApp />;
-}
--- pages/_app.page.jsx
+++
@@ -1,5 +0,0 @@
-import '../styles/globals.css';
-
-export default function NextApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
